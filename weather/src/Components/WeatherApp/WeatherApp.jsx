1303956a73b730087a15b8bdb4645361814c3d5c
Grossholz Michele <Michele.Grossholz@swisscom.com>
2024-04-22 13:53:32 +0200

    Man kann nun auch direkt im Input feld Enter drücken und suchen

--- weather/src/Components/WeatherApp/WeatherApp.jsx
+++ weather/src/Components/WeatherApp/WeatherApp.jsx
@@ -61,10 +61,19 @@ const WeatherApp = () => {
         }
     }
 
+    function handleKeyDown(event){
+        if(event.key === 'Enter'){
+            search();
+        }
+    }
+
     return (
         <div className="container">
             <div className="top-bar">
-                <input type="text" className="cityInput" placeholder="Search" />
+                <input type="text" className="cityInput" placeholder="Search" onKeyDown={handleKeyDown}/>
+
+                    
+
                 <div className="search-icon" onClick={() => {search()}}>
                     <img src={search_icon} alt="" />
                 </div>
